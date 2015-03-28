614484fe63797a25502fe6c2a1b465a77c3ddfe1
Ryan Plessner <plessner.ryan@gmail.com>
2015-03-28 18:36:56 -0400

    animated hold position for chicken legs. also flipping position

--- js/Preload.js
+++ js/Preload.js
@@ -16,7 +16,8 @@ SideScroller.Preload.prototype = {
     this.load.tilemap('level1', 'assets/tilemaps/level1.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image('gameTiles', 'assets/images/tiles_spritesheet.png');
     this.load.image('eye', 'assets/images/eye.png');
-    this.load.spritesheet('eye-walk', 'assets/images/eyewalk/eye-walk-cycle.png', 90, 115);
+    this.load.spritesheet('eye-walk', 'assets/images/eyewalk/eye-walk-cycle.png', 90, 97);
+    this.load.spritesheet('eye-hold', 'assets/images/eyehold/eye-hold-cycle.png', 71, 90);
     this.load.image('playerDead', 'assets/images/player_dead.png');
     this.load.image('goldCoin', 'assets/images/goldCoin.png');
     // audio
--- js/monsters.js
+++ js/monsters.js
@@ -24,8 +24,9 @@ Eyeball.prototype = {
 function Kangaroo(Game) {
   this.player = Game.player;
   this.player.rotation = 0;
-  this.player.loadTexture('eye-walk', 0);
   this.player.animations.add('eye-walk', [5, 4, 3, 2, 1]);
+  this.player.animations.add('eye-hold', [1, 2, 3, 4]);
+  this._hold_animation();
   this.jumpSound = Game.add.audio('jump');
 }
 
@@ -33,7 +34,7 @@ Kangaroo.prototype = {
   update: function(cursors) {
     if (cursors.left.isDown) {
       this.player.body.velocity.x = -300;
-      this.player.animations.play('eye-walk', 12, true);
+      this._walk_animation();
       if (this.player.scale.x > 0) {
         this.player.scale.x *= -1;
       }
@@ -41,7 +42,7 @@ Kangaroo.prototype = {
 
     if (cursors.right.isDown) {
       this.player.body.velocity.x = 300;
-      this.player.animations.play('eye-walk', 12, true);
+      this._walk_animation();
       if (this.player.scale.x < 0) {
         this.player.scale.x *= -1;
       }
@@ -49,12 +50,28 @@ Kangaroo.prototype = {
 
     if (!cursors.left.isDown && !cursors.right.isDown) {
       this.player.body.velocity.x = 0;
-      this.player.animations.stop(null, true);
+      this._hold_animation();
     }
 
     if(cursors.up.isDown && this.player.body.blocked.down) {
       this.player.body.velocity.y -= 700;
       this.jumpSound.play();
+    }
+  },
+
+  _hold_animation: function() {
+    if (!this.player.animations.currentAnim ||
+        this.player.animations.currentAnim.name != 'eye-hold') {
+      this.player.loadTexture('eye-hold', 0);
+      this.player.animations.play('eye-hold', 4, true);
+    }
+  },
+
+  _walk_animation: function() {
+    if (!this.player.animations.currentAnim ||
+        this.player.animations.currentAnim.name != 'eye-walk') {
+      this.player.loadTexture('eye-walk', 0);
+      this.player.animations.play('eye-walk', 12, true);
     }
   }
 };
